7107f5e8a8d2563af40a98888967bfd782a8c663
Dudesendhelp <zoomassembly@gmail.com>
2026-03-12 09:01:52 +0530

    added udf to calculate the factorial

--- factorial.cpp
+++ factorial.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
 using namespace std;
 
+int factorial(int n)
+{
+    int i;
+    int m=1;
+    for(i=1;i<=n;i++)
+    {
+        m=m*i;
+    }
+    return m;
+}
 int main()
 {
     int i;
     int a;
     cout<< "Enter the number to find its factorial\n";
     cin>>a;
-    int m=1;
-    for(i=1;i<=a;i++)
-    {
-        m=m*i;
-    }
+    int m=factorial(a);
     cout<<"Factorial = "<<m<<"\n";
     return 0;
 }
